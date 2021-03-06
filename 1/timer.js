9df2933118d2c869165e72a065affc749d0b416d
huangdh <huangdh@huangdhdeMacBook-Pro.local>
2021-03-06 13:51:54 +0800

    nodejs-column-first-commit

--- 1/timer.js
+++
@@ -1,7 +0,0 @@
-const fs = require(fs);
-
-setTimeout(() => {
-    console.log('1');
-}, 0);
-
-console.log('2');
--- 12/cpu/lib/md5.js
+++
@@ -1,5 +0,0 @@
-crypto = require('crypto');
-
-module.exports = (content) => { 
-    return crypto.createHash('md5').update(content).digest("hex");
-}
