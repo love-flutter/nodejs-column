0f0e79414e67f32819fcef943e415003710f966f
492383469@qq.com <huangdh@huangdhdeMacBook-Pro.local>
2021-04-21 09:55:41 +0800

    update-check-token

--- 18/src/middleware/checkToken.js
+++ 18/src/middleware/checkToken.js
@@ -9,13 +9,13 @@ const tokenMapping = {
 
 module.exports = function () {
     return async function ( ctx, next ) {
-        // const params = querystring.parse(ctx.request.querystring);
-        // let token = ctx.request.headers.token || params['token'];
-        // if(!token && !tokenMapping[token]){
-        //     return baseFun.setResInfo(ctx, false, 'pls login first', null, 401); 
-        // }    
-       // ctx.userId = tokenMapping[token];
-       ctx.userId = `${ Math.floor(Math.random() * (10000000 - 1000) + 1000)}`;
+        const params = querystring.parse(ctx.request.querystring);
+        let token = ctx.request.headers.token || params['token'];
+        if(!token && !tokenMapping[token]){
+            return baseFun.setResInfo(ctx, false, 'pls login first', null, 401); 
+        }    
+       ctx.userId = tokenMapping[token];
+       //ctx.userId = `${ Math.floor(Math.random() * (10000000 - 1000) + 1000)}`;
         await next();
     }
 }
