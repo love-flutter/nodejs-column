dbf73606cd998a882c77dfbc60920c6f30dbae0b
492383469@qq.com <huangdh@huangdhdeMacBook-Pro.local>
2021-04-23 22:45:51 +0800

    add-koa-serverless

--- 19/app.js
+++ 19/app.js
@@ -17,4 +17,9 @@ app.use(koaBody({
 app.use(routerMiddleware());
 //app.use(backendRouter());
 
+if(process.env.SERVERLESS) {
+    module.exports = app 
+  } else {
+    app.listen(3000) 
+  }
 //app.listen(3000, () => console.log(`Example app listening on port 3000!`));
